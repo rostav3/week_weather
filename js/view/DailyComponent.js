0303e7db7b04766b1515cc8b4e23ae8571791fa9
stav rockah <stav.r.24@gmail.com>
2019-08-31 20:43:58 +0300

    add events

--- js/view/DailyComponent.js
+++ js/view/DailyComponent.js
@@ -1,6 +1,9 @@
-class DailyComponent{
-          constructor(day){
+class DailyComponent {
+          constructor(day) {
                     let main = document.getElementById("main");
+                    let event = new Event("hello", { bubbles: true }); // (2)
+                    main.dispatchEvent(event);
+
                     // TODO: handle daily view (enter to main.innerHTML)
           }
 }
--- js/view/WeatherComponent.js
+++ js/view/WeatherComponent.js
@@ -1,6 +1,10 @@
 class WeatherComponent{
           constructor(week){
                     let main = document.getElementById("main");
+                    main.addEventListener("hello", function(event) { // (1)
+                              alert("Hello from " + event.target.tagName); // Hello from H1
+                            });
+                          
                     for (let i = 0; i < week.length; i++) {
                               new DailyComponent(week[i]);
                     }
